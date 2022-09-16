40268d405e10c0904e65c3009fd9f554ca27503e
Renesa Ray <rayrenesa@gmail.com>
2022-09-16 23:11:24 +0530

    Add files via upload

---
+++ front-end/projects/lib_demo/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AngularFileUploaderModule } from "angular-file-uploader";
+//import { AppRoutingModule } from './.app-routing.module'
+// To use angular-file-uploader from node-modules remove paths from tsconfig.base.json in root.
+/*
+"paths": {
+      "angular-file-uploader": [
+        "dist/angular-file-uploader"
+      ],
+      "angular-file-uploader/*": [
+        "dist/angular-file-uploader/*"
+      ]
+    }
+*/
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AngularFileUploaderModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppModule { }
